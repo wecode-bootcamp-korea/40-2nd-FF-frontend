352d24d40c5b63c9b9e3e3f791e6be6a75006cb2
metamro <en160305@gmail.com>
2022-12-23 21:38:05 +0900

    Modify: 초기세팅 수정

--- src/Router.js
+++ src/Router.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
+import GourmetDetail from './pages/GourmetDetail/GourmetDetail';
+import GourmetList from './pages/GourmetList/GourmetList';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/main" element={<Main />} />
+        <Route path="/Detail" element={<GourmetDetail />} />
+        <Route path="/List" element={<GourmetList />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
