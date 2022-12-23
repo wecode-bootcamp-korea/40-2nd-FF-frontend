d84af008916568fdfde095d8c5b3594e7475ea9e
metamro <en160305@gmail.com>
2022-12-23 21:03:04 +0900

    Modify: 초기세팅 수정

---
+++ src/pages/GourmetDetail/GourmetDetail.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GourmetDetail = () => {
+  return <h1>GourmetDetail</h1>;
+};
+
+export default GourmetDetail;
---
+++ src/pages/GourmetList/GourmetList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GourmetList = () => {
+  return <h1>GourmetList</h1>;
+};
+
+export default GourmetList;
